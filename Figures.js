769f9ba256faa2b84434488dce83b716218b5118
Robin Bäuerlein <robin.baeuerlein@gmail.com>
2019-05-05 14:26:48 +0200

    Implemented move-sets for figures for all figures based on common chess rules

--- Figures.js
+++ Figures.js
@@ -5,6 +5,7 @@ class Figure{
 		this.team = team;
 		this.type = this.constructor.name;
 		this.moveCount = 0;
+		this.isAlive = true;
 	}
 
 	determineMovePossibilities(){
@@ -15,14 +16,15 @@ class Figure{
 		this.positionX = x;
 		this.positionY = y;
 		this.moveCount++;
+
+		if(this.type === "Peasant" && (this.positionY === 7 || this.positionY === 0)) return true;
 	}
 }
 
 class Peasant extends Figure{
 	constructor(positionX, positionY, team){
 		super(positionX, positionY, team);
-		//this.asset = (this.team === 1) ? 'whitePeasant' : 'blackPeasant';
-		this.asset = 'whitePeasant';
+		this.asset = (this.team === 1) ? 'whitePeasant' : 'blackPeasant';
 	}
 
 	determineMovePossibilities(){
@@ -37,45 +39,85 @@ class Peasant extends Figure{
 			y: 1
 		};
 	}
+
+	transform(type){
+
+	}
+
 }
 
 class Jumper extends Figure{
 	constructor(positionX, positionY, team){
 		super(positionX, positionY, team);
-		//this.asset = (this.team === 1) ? 'whiteJumper' : 'blackJumper';
-		this.asset = 'whiteJumper';
+		this.asset = (this.team === 1) ? 'whiteJumper' : 'blackJumper';
+	}
+	determineMovePossibilities(){
+		return{
+			ne:[{x: 2, y: -1}, {x: 1, y: -2}],
+			es:[{x: 2, y: 1}, {x: 1, y: 2}],
+			sw:[{x: -1, y: 2}, {x: -2, y: 1}],
+			nw:[{x: -2, y: -1}, {x: -1, y: -2}]
+		};
 	}
 }
 
 class Runner extends Figure{
 	constructor(positionX, positionY, team){
 		super(positionX, positionY, team);
-		//this.asset = (this.team === 1) ? 'whiteJumper' : 'blackJumper';
-		this.asset = 'whiteRunner';
+		this.asset = (this.team === 1) ? 'whiteRunner' : 'blackRunner';
+	}
+	determineMovePossibilities(){
+		// return{
+		// 	ne: this.positionY - this.positionX,
+		// 	es: (this.positionX < this.positionY) ? 7 - this.positionY : this.positionX - this.positionX,
+		// 	sw: (this.positionX < this.positionY) ? 7 - this.positionY - this.positionY : this.positionX - this.positionX,
+		// 	nw: (this.positionX < this.positionY) ? this.positionX : this.positionX
+		// };
+		return ['ne', 'es', 'sw', 'nw'];
 	}
 }
 
 class Tower extends Figure{
 	constructor(positionX, positionY, team){
 		super(positionX, positionY, team);
-		//this.asset = (this.team === 1) ? 'whiteTower' : 'blackTower';
-		this.asset = 'whiteTower';
+		this.asset = (this.team === 1) ? 'whiteTower' : 'blackTower';
 	}
-
+	determineMovePossibilities(){
+		return{
+			up: this.positionY,
+			down: 7 - this.positionY,
+			left: this.positionX,
+			right: 7 - this.positionX
+		};
+	}
 }
 
 class Lady extends Figure{
 	constructor(positionX, positionY, team){
 		super(positionX, positionY, team);
-		//this.asset = (this.team === 1) ? 'whiteLady' : 'blackLady';
-		this.asset = 'whiteLady';
+		this.asset = (this.team === 1) ? 'whiteLady' : 'blackLady';
+	}
+	determineMovePossibilities(){
+		return{
+			tower: {
+				up: this.positionY,
+				down: 7 - this.positionY,
+				left: this.positionX,
+				right: 7 - this.positionX
+			},
+			runner: {
+				directions: ['ne', 'es', 'sw', 'nw']
+			}
+		};
 	}
 }
 
 class King extends Figure{
 	constructor(positionX, positionY, team){
 		super(positionX, positionY, team);
-		//this.asset = (this.team === 1) ? 'whiteKing' : 'blackKing';
-		this.asset = 'whiteKing';
+		this.asset = (this.team === 1) ? 'whiteKing' : 'blackKing';
+	}
+	determineMovePossibilities(){
+		return;
 	}
 }
--- LocalGame.js
+++ LocalGame.js
@@ -9,12 +9,20 @@ class LocalGame extends Phaser.Scene {
 		this.load.image('whiteTile', 'assets/whiteTile.png');
 		this.load.image('brownTile', 'assets/brownTile.png');
 		this.load.image('focus', 'assets/focus.png');
+		this.load.image('hit', 'assets/hit.png');
 		this.load.image('whitePeasant', 'assets//figures/whitePeasant.png');
 		this.load.image('whiteJumper', 'assets/figures/whiteJumper.png');
 		this.load.image('whiteKing', 'assets/figures/whiteKing.png');
 		this.load.image('whiteLady', 'assets/figures/whiteLady.png');
 		this.load.image('whiteRunner', 'assets/figures/whiteRunner.png');
 		this.load.image('whiteTower', 'assets/figures/whiteTower.png');
+
+		this.load.image('blackPeasant', 'assets//figures/blackPeasant.png');
+		this.load.image('blackJumper', 'assets/figures/blackJumper.png');
+		this.load.image('blackKing', 'assets/figures/blackKing.png');
+		this.load.image('blackLady', 'assets/figures/blackLady.png');
+		this.load.image('blackRunner', 'assets/figures/blackRunner.png');
+		this.load.image('blackTower', 'assets/figures/blackTower.png');
 		this.boardMatrix = new Array(8);
 		for (let i = 0; i < this.boardMatrix.length; i++) {
 			this.boardMatrix[i] = [];
@@ -56,8 +64,8 @@ class LocalGame extends Phaser.Scene {
 				this.add.sprite(tile.x, tile.y, tile.ground);
 			})
 		})
-		this.startLineup(this.player1);
-		this.startLineup(this.player2);
+		this.initFiguresOnMatrix(this.player1);
+		this.initFiguresOnMatrix(this.player2);
 		this.turn = 1;
 		document.getElementById('turn').innerHTML = "Aktueller Spieler: <b> Spieler " + this.turn + "</b>";
 		this.focus = this.add.sprite(10000, 10000, 'focus');
@@ -71,13 +79,21 @@ class LocalGame extends Phaser.Scene {
 				return;
 			}
 			//Triggers when the user moves a figure
-			else if(this.focusedTile !== null && clickedTile.figure === null && clickedTile.moveable === true){
+			else if(this.focusedTile !== null && clickedTile.figure === null && clickedTile.moveable){
+				this.move(clickedTile);
+				return;
+			}
+			//Triggers when the user wants to beat another figure
+			else if(clickedTile.figure !== null && clickedTile.figure.figure.hitable){
+				this.removeFigure(clickedTile.figure.figure);
 				this.move(clickedTile);
 				return;
 			}
 			//Triggers when the user clicks on an opponents figure or an empty tile
-			//TODO: Beat figures
-			else if (clickedTile.figure === null || clickedTile.figure.figure.team !== this.turn) return;
+			else if (clickedTile.figure === null || clickedTile.figure.figure.team !== this.turn){ 
+				this.defocus();
+				return
+			};
 			this.focusFigure(clickedTile);
 		})
 	}
@@ -97,12 +113,17 @@ class LocalGame extends Phaser.Scene {
 	 @param clickedTile: Tile the user wants to move the figure
 	 */
 	 move(newTile){
-	 	this.focusedTile.figure.figure.move(newTile.indX, newTile.indY);
+	 	let isTransforming = this.focusedTile.figure.figure.move(newTile.indX, newTile.indY);
 	 	newTile.figure = this.focusedTile.figure;
 	 	this.focusedTile.figure = null;
 	 	newTile.figure.sprite.x = this.boardMatrix[newTile.figure.figure.positionY][newTile.figure.figure.positionX].x;
 	 	newTile.figure.sprite.y = this.boardMatrix[newTile.figure.figure.positionY][newTile.figure.figure.positionX].y;
 	 	this.defocus();
+
+	 	if(isTransforming){
+	 		newTile.figure.figure.transform();
+	 	}
+
 	 	document.getElementById('lastTurn').innerHTML = "<b>" + newTile.figure.figure.type + "</b> von <b> Spieler" + newTile.figure.figure.team + "</b> auf Feld <b>" + newTile.designation + "</b> verschoben!";
 	 	this.changePlayerTurn();
 	 }
@@ -119,9 +140,10 @@ class LocalGame extends Phaser.Scene {
 	 moveableTile(figure){
 	 	let moveable = figure.determineMovePossibilities();
 	 	this.moveableSprites = new Array();
-	 	let x = this["player" + this.turn]["apply" + figure.type + "Possibilities"]();
-	 	let apply = x.bind(this, figure, moveable);
-	 	apply();
+	 	this.hitmarker = new Array();
+	 	let x = this["player" + figure.team]["apply" + figure.type + "Possibilities"]();
+	 	x.call(this, figure, moveable);
+	 	this.transparentMarker();
 	 }
 
 	 defocus(){
@@ -138,7 +160,19 @@ class LocalGame extends Phaser.Scene {
 	 		})
 	 	})
 
+	 	this.boardMatrix.forEach(row => {
+	 		row.forEach(tile => {
+	 			if (tile.figure !== null){
+	 				tile.figure.figure.hitable = false;
+	 			}
+	 		})
+	 	})
+
 	 	this.moveableSprites.forEach(sprite => {
+	 		sprite.destroy();
+	 	})
+
+	 	this.hitmarker.forEach(sprite => {
 	 		sprite.destroy();
 	 	})
 	 }
@@ -148,7 +182,22 @@ class LocalGame extends Phaser.Scene {
 	 	document.getElementById('turn').innerHTML = "Aktueller Spieler: <b> Spieler " + this.turn + "</b>";
 	 }
 
-	 startLineup(player){
+	 removeFigure(figure){
+	 	this.boardMatrix[figure.positionY][figure.positionX].figure.sprite.destroy();
+	 	this.boardMatrix[figure.positionY][figure.positionX].figure = null;
+	 	figure.isAlive = false;
+	 }
+
+	 transparentMarker(){
+	 	this.moveableSprites.forEach(moveableFocus =>{
+			moveableFocus.alpha -= 0.7;
+		})
+		this.hitmarker.forEach(hit =>{
+			hit.alpha -= 0.2;
+		})
+	 }
+
+	 initFiguresOnMatrix(player){
 	 	Object.values(player.figureList).forEach(type => {
 	 		type.forEach(figure => {
 	 			let tile = this.boardMatrix[figure.positionY][figure.positionX];
@@ -158,8 +207,46 @@ class LocalGame extends Phaser.Scene {
 	 			};
 	 		})
 	 	})
-	 }
 	}
+
+	tilesAroundFigure(figure){
+		let x = new Array();
+		let operators = {
+			'+': function(a, b){return a + b},
+			'-': function(a, b){return a - b},
+			'=': function(a, b){return a}
+		}
+		for(let i = 0; i < 8; i++){
+			let firstOperator;
+			let secondOperator;
+			if(i === 0 || i === 4){
+				firstOperator = operators['='];
+				if(i === 0) secondOperator = operators['-'];
+				else if(i === 4) secondOperator = operators['+'];
+			}
+			else if(i <= 3) {
+				firstOperator = operators['+'];
+				if(i === 1) secondOperator = operators['-'];
+				else if(i === 2) secondOperator = operators['='];
+				else if(i === 3) secondOperator = operators['+'];
+			}
+			else{
+				firstOperator = operators['-'];
+				if(i === 5) secondOperator = operators['+'];
+				else if(i === 6) secondOperator = operators['='];
+				else if(i === 7) secondOperator = operators['-'];
+			}
+
+			try{
+				let tile = this.boardMatrix[firstOperator(figure.positionY, 1)][secondOperator(figure.positionX, 1)];
+				x.push(tile);
+			}
+			catch(err){}
+		}
+		return x;
+	}
+}
+
 
 class Player{
 	constructor(number){
@@ -170,12 +257,12 @@ class Player{
 			this.number === 1 ? peasantList.push(new Peasant(i, startLane - 1, 1)) : peasantList.push(new Peasant(i, startLane + 1, 2));
 		}
 		this.figureList = {
-			peasant: peasantList,
-			tower: [new Tower(0, startLane, this.number), new Tower(7, startLane, this.number)],
-			jumper: [new Jumper(1, startLane, this.number), new Jumper(6, startLane, this.number)],
-			runner: [new Runner(2, startLane, this.number), new Runner(5, startLane, this.number)],
-			lady: [new Lady(3, startLane, this.number)],
-			king: [new King(4, startLane, this.number)]
+			Peasant: peasantList,
+			Tower: [new Tower(0, startLane, this.number), new Tower(7, startLane, this.number)],
+			Jumper: [new Jumper(1, startLane, this.number), new Jumper(6, startLane, this.number)],
+			Runner: [new Runner(2, startLane, this.number), new Runner(5, startLane, this.number)],
+			Lady: [new Lady(3, startLane, this.number)],
+			King: [new King(4, startLane, this.number)]
 		};
 	}
 
@@ -184,27 +271,259 @@ class Player{
 			case 1:
 			return function(figure, moveable) {
 				for(let i = 1; i <= moveable.y; i++){
-					let tile = this.boardMatrix[figure.positionY - i][figure.positionX];
+					var tile = this.boardMatrix[figure.positionY - i][figure.positionX];
 					if(tile.figure !== null) break;
 					tile.moveable = true;
 					this.moveableSprites.push(this.add.sprite(tile.x, tile.y, 'focus'));
 				}
-				for(let moveableFocus of this.moveableSprites){
-					moveableFocus.alpha -= 0.7;
+
+				//Check if peasant can potentially beat an opponents figure
+				if(figure.positionX < 7){
+					tile = this.boardMatrix[figure.positionY - 1][figure.positionX + 1];
+						if(tile.figure !== null && tile.figure.figure.team !== figure.team){
+							tile.figure.figure.hitable = true;
+							this.hitmarker.push(this.add.sprite(tile.x, tile.y, 'hit'));
+						}
+				}
+
+				if(figure.positionX > 0){
+					tile = this.boardMatrix[figure.positionY - 1][figure.positionX - 1];
+					if(tile.figure !== null && tile.figure.figure.team !== figure.team){
+						tile.figure.figure.hitable = true;
+						this.hitmarker.push(this.add.sprite(tile.x, tile.y, 'hit'));
+					}
 				}
 			}
 			case 2:
 			return function(figure, moveable) {
 				for(let i = 1; i <= moveable.y; i++){
-					let tile = this.boardMatrix[figure.positionY + i][figure.positionX];
+					var tile = this.boardMatrix[figure.positionY + i][figure.positionX];
 					if(tile.figure !== null) break;
 					tile.moveable = true;
 					this.moveableSprites.push(this.add.sprite(tile.x, tile.y, 'focus'));
 				}
-				for(let moveableFocus of this.moveableSprites){
-					moveableFocus.alpha -= 0.7;
+				
+				//Check if peasant can potentially beat an opponents figure
+				if(figure.positionX < 7){
+					tile = this.boardMatrix[figure.positionY + 1][figure.positionX + 1];
+					if(tile.figure !== null && tile.figure.figure.team !== figure.team){
+						tile.figure.figure.hitable = true;
+						this.hitmarker.push(this.add.sprite(tile.x, tile.y, 'hit'));
+					}
+				}
+				if(figure.positionX > 0){
+					tile = this.boardMatrix[figure.positionY + 1][figure.positionX - 1];
+					if(tile.figure !== null && tile.figure.figure.team !== figure.team){
+						tile.figure.figure.hitable = true;
+						this.hitmarker.push(this.add.sprite(tile.x, tile.y, 'hit'));
+					}
 				}
 			}
+		}
+	}
+
+	applyTowerPossibilities(){
+		return function(figure, moveable){
+			let chooseTile = function(direction, i){
+				switch(direction){
+					case 'up':
+						return this.boardMatrix[figure.positionY - i][figure.positionX];
+					case 'down':
+						return this.boardMatrix[figure.positionY + i][figure.positionX];
+					case 'left':
+						return this.boardMatrix[figure.positionY][figure.positionX - i];
+					case 'right':
+						return this.boardMatrix[figure.positionY][figure.positionX + i];
+					default:
+						throw new Error('Couldn\'t locate the tile. ' + direction + 'Iteration: ' + i);
+				}
+			}
+			for(let direction in moveable){
+				for(let i = 1; i <= moveable[direction]; i++){
+					let tile = chooseTile.call(this, direction, i);
+					if(tile.figure !== null){
+						if(tile.figure.figure.team !== figure.team){
+							tile.figure.figure.hitable = true;
+							this.hitmarker.push(this.add.sprite(tile.x, tile.y, 'hit'));
+							break;
+						}
+						else{
+							break;
+						}
+					}
+					tile.moveable = true;
+					this.moveableSprites.push(this.add.sprite(tile.x, tile.y, 'focus'));
+				}
+			}
+		}
+	}
+	applyRunnerPossibilities(){
+		return function(figure, moveable){
+			let chooseTile = function(direction, i){
+				switch(direction){
+					case 'ne':
+						return this.boardMatrix[figure.positionY - i][figure.positionX + i];
+					case 'es':
+						return this.boardMatrix[figure.positionY + i][figure.positionX + i];
+					case 'sw':
+						return this.boardMatrix[figure.positionY + i][figure.positionX - i];
+					case 'nw':
+						return this.boardMatrix[figure.positionY - i][figure.positionX - i];
+					default:
+						throw new Error('Couldn\'t locate the tile. ' + direction + 'Iteration: ' + i);
+				}
+			}
+			moveable.forEach(direction =>{
+				for(let i = 1; true; i++){
+					let tile;
+					try{
+						tile = chooseTile.call(this, direction, i);
+					}
+					catch(err){}
+					if(tile === undefined) break;
+					if(tile.figure !== null){
+						if(tile.figure.figure.team !== figure.team){
+							tile.figure.figure.hitable = true;
+							this.hitmarker.push(this.add.sprite(tile.x, tile.y, 'hit'));
+							break;
+						}
+						break;
+					}
+					tile.moveable = true;
+					this.moveableSprites.push(this.add.sprite(tile.x, tile.y, 'focus'));
+				}
+			})
+		}
+	}
+
+	applyJumperPossibilities(){
+		return function(figure, moveable){
+			for(let direction in moveable){
+				for(let tilePosition of moveable[direction]){
+					let tile;
+					try{
+						tile = this.boardMatrix[figure.positionY + tilePosition.y][figure.positionX + tilePosition.x];
+					}
+					catch(err){}
+					if(tile === undefined) continue;
+					if(tile.figure !== null){
+						if(tile.figure.figure.team !== figure.team){
+							tile.figure.figure.hitable = true;
+							this.hitmarker.push(this.add.sprite(tile.x, tile.y, 'hit'));
+							continue;
+						}
+						continue;
+					}
+					tile.moveable = true;
+					this.moveableSprites.push(this.add.sprite(tile.x, tile.y, 'focus'));
+				}
+			}
+		}
+	}
+
+	applyKingPossibilities(){
+		return function(figure){
+			let tiles = this.tilesAroundFigure(figure);
+			tiles.forEach(tile =>{
+				if(tile.figure !== null){
+					if(tile.figure.figure.team !== figure.team){
+						tile.figure.figure.hitable = true;
+						this.hitmarker.push(this.add.sprite(tile.x, tile.y, 'hit'));
+						return;
+					}
+					return;
+				}
+				tile.moveable = true;
+				this.moveableSprites.push(this.add.sprite(tile.x, tile.y, 'focus'));
+			})
+		}
+	}
+
+	applyLadyPossibilities(){
+		return function(figure, moveable){
+			let tiles = this.tilesAroundFigure(figure);
+			tiles.forEach(tile =>{
+				if(tile.figure !== null){
+					if(tile.figure.figure.team !== figure.team){
+						tile.figure.figure.hitable = true;
+						this.hitmarker.push(this.add.sprite(tile.x, tile.y, 'hit'));
+						return;
+					}
+					return;
+				}
+				tile.moveable = true;
+				this.moveableSprites.push(this.add.sprite(tile.x, tile.y, 'focus'));
+			})
+
+			let chooseTile = function(direction, i){
+				switch(direction){
+					case 'up':
+						return this.boardMatrix[figure.positionY - i][figure.positionX];
+					case 'down':
+						return this.boardMatrix[figure.positionY + i][figure.positionX];
+					case 'left':
+						return this.boardMatrix[figure.positionY][figure.positionX - i];
+					case 'right':
+						return this.boardMatrix[figure.positionY][figure.positionX + i];
+					default:
+						throw new Error('Couldn\'t locate the tile. ' + direction + 'Iteration: ' + i);
+				}
+			}
+			console.log(moveable.tower);
+			for(let direction in moveable.tower){
+				for(let i = 1; i <= moveable.tower[direction]; i++){
+					let tile = chooseTile.call(this, direction, i);
+					if(tile.figure !== null){
+						if(tile.figure.figure.team !== figure.team){
+							tile.figure.figure.hitable = true;
+							this.hitmarker.push(this.add.sprite(tile.x, tile.y, 'hit'));
+							break;
+						}
+						else{
+							break;
+						}
+					}
+					if(tile.moveable) continue;
+					tile.moveable = true;
+					this.moveableSprites.push(this.add.sprite(tile.x, tile.y, 'focus'));
+				}
+			}
+
+			chooseTile = function(direction, i){
+				switch(direction){
+					case 'ne':
+						return this.boardMatrix[figure.positionY - i][figure.positionX + i];
+					case 'es':
+						return this.boardMatrix[figure.positionY + i][figure.positionX + i];
+					case 'sw':
+						return this.boardMatrix[figure.positionY + i][figure.positionX - i];
+					case 'nw':
+						return this.boardMatrix[figure.positionY - i][figure.positionX - i];
+					default:
+						throw new Error('Couldn\'t locate the tile. ' + direction + 'Iteration: ' + i);
+				}
+			}
+			moveable.runner.directions.forEach(direction =>{
+				for(let i = 1; true; i++){
+					let tile;
+					try{
+						tile = chooseTile.call(this, direction, i);
+					}
+					catch(err){}
+					if(tile === undefined) break;
+					if(tile.figure !== null){
+						if(tile.figure.figure.team !== figure.team){
+							tile.figure.figure.hitable = true;
+							this.hitmarker.push(this.add.sprite(tile.x, tile.y, 'hit'));
+							break;
+						}
+						break;
+					}
+					if(tile.moveable) continue;
+					tile.moveable = true;
+					this.moveableSprites.push(this.add.sprite(tile.x, tile.y, 'focus'));
+				}
+			})
 		}
 	}
 }
